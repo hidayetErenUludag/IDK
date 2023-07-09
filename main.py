73ab8b20fb5726ccf42ef61b70bdafb5b0241e93
Hidayet Eren Uludağ <76475118+mightymountain@users.noreply.github.com>
2023-07-09 12:15:44 +0300

    ıdk

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import requests
 login = "http://192.168.1.14:8080/jsonrpc"
 username = "admin"
 password = "admin"
-
+service_vpn3 = "http://192.168.1.14:8080/webui-one/ServiceManager/l3vpn:vpn/l3vpn"
+service_commit = "http://192.168.1.14:8080/webui-one/CommitManager"
 session = requests.session()
 
 
@@ -14,18 +15,57 @@ def loger(user, passw):
 
     if response.status_code == 200 and "result" in response.json():
         print("Login Successful! ")
-        print(session.cookies.values())
-        return True
+        print(response.cookies.get("sessionid_8080"))
+        return response.cookies.get("sessionid_8080")
     else:
-        print("Login failed.")
-        return False
+        return "Login failed."
 
 
-def get_trans():
-    print(loger(username, password))
-    response = session.get("http://192.168.1.14:8080/webui-one/ServiceManager/l3vpn:vpn/l3vpn",
-                           json={"jsonrpc": "2.0", "method": "get_trans", "params": {}, "id": 289})
-    print(response.url)
+def create_service():
+    sessionid = loger(username, password)
+    x = {"Cookie": sessionid,
+         "method": "login"}
+
+    configuration = {
+        "l3vpn": {
+            "name": "volvo",
+            "as-number": "65101",
+            "endpoints": [
+                {
+                    "name": "main-office",
+                    "ce-device": "ce0",
+                    "ce-interface": "GigabitEthernet0/11",
+                    "ip-network": "10.10.1.0/24",
+                    "bandwidth": "12000000"
+                },
+                {
+                    "name": "branch-office1",
+                    "ce-device": "ce1",
+                    "ce-interface": "GigabitEthernet0/11",
+                    "ip-network": "10.7.7.0/24",
+                    "bandwidth": "6000000"
+                },
+                {
+                    "name": "branch-office2",
+                    "ce-device": "ce4",
+                    "ce-interface": "GigabitEthernet0/18",
+                    "ip-network": "10.8.8.0/24",
+                    "bandwidth": "300000"
+                }
+            ],
+            "qos-policy": "GOLD"
+        }
+    }
+    print(x)
+
+    response = requests.post(service_vpn3, json=configuration, headers=x)
+
+    if response.status_code == 201:
+        print("Configuration created successfully!")
+    else:
+        print(f"Failed to create configuration. Status code: {response.status_code}")
+        print("Response content:")
+        print(response)
 
 
-get_trans()
+create_service()
