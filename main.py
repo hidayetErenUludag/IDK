037b37a9d308dd7502d0372fcec277c02387b73f
Hidayet Eren Uludağ <76475118+mightymountain@users.noreply.github.com>
2023-07-10 12:40:20 +0300

    real_deal

--- main.py
+++ main.py
@@ -1,90 +1,186 @@
+import json
 import requests
 
-login = "http://192.168.1.14:8080/jsonrpc"
-username = "admin"
-password = "admin"
-service_vpn3 = "http://192.168.1.14:8080/webui-one/ServiceManager"
-service_commit = "http://192.168.1.14:8080/webui-one/CommitManager"
-session = requests.session()
+# Define the URL for the JSON-RPC endpoint
+url = "http://192.168.1.15:8080/jsonrpc"
 
+# Prepare payload for authentication request
+payload = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "login",
+    "params": {"user": "admin", "passwd": "admin"}
+}
 
-def loger(user, passw):
-    response = session.post(login,
-                            json={"jsonrpc": "2.0", "method": "login", "params": {"user": user, "passwd": passw},
-                                  "id": 2})
+# Send authentication request to obtain cookies
+authenticate = requests.post(url, json=payload, verify=False)
+cookies_string = authenticate.cookies
 
-    if response.status_code == 200 and "result" in response.json():
-        print("Login Successful! ")
-        print(response.cookies.get("sessionid_8080"))
-        print(response.headers.get("Allow"))
-        return response.cookies.get("sessionid_8080")
-    else:
-        return "Login failed."
+# Start a new read transaction
+payload = {
+    "jsonrpc": "2.0",
+    "id": 2,
+    "method": "new_trans",
+    "params": {"db": "running", "mode": "read"}
+}
 
+# Send the request with the cookies from authentication
+req = requests.post(url, cookies=cookies_string, json=payload, verify=False)
+response = req.text
+th_id = json.loads(response)["result"]["th"]
 
-def run_action():
-    sessionid = loger(username, password)
-    x = {
-        "Cookie": "sessionid_8080=" + sessionid,
-        "method": "GET"
+# Start a new read-write transaction
+payload = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "new_trans",
+    "params": {"db": "running", "mode": "read_write"}
+}
+
+# Send the request with the cookies from authentication
+req = requests.post(url, cookies=cookies_string, json=payload, verify=False)
+response = req.text
+th_id = json.loads(response)["result"]["th"]
+
+# Get the transaction information
+payload = {"jsonrpc": "2.0", "id": 11, "method": "get_trans"}
+req = requests.post(url, cookies=cookies_string, json=payload, verify=False)
+req.text
+
+# Get the list of keys for ztp-fttb entries
+payload = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "get_list_keys",
+    "params": {"th": th_id, "path": "/ztp-fttb:ztp-fttb"}
+}
+
+req = requests.post(url, cookies=cookies_string, json=payload, verify=False)
+response = req.text
+ztp_entries = json.loads(response)["result"]["keys"]
+
+# Get specific values for a ztp-fttb entry
+payload = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "get_values",
+    "params": {
+        "th": th_id,
+        "path": "/ztp-fttb:ztp-fttb{1.1.1.1}",
+        "leafs": ["dhcp_ip", "subnet", "device_model"]
     }
+}
 
+req = requests.post(url, cookies=cookies_string, json=payload, verify=False)
+response = req.text
+ztp_entry = json.loads(response)
+ztp_entry["result"]["values"][0]["value"]
 
-    response = session.post(service_vpn3,
-                            json={"jsonrpc": "2.0",
-                                  "method": "run_action",
-                                  "params": {"th": 2, "path": "/l3vpn:vpn/l3vpn{volvo}/check-sync"}})
-
-
-def create_service():
-    sessionid = loger(username, password)
-    x = {
-        "Cookie": "sessionid_8080=" + sessionid,
-        "method": "GET"
+# Get values for another ztp-fttb entry
+payload = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "get_values",
+    "params": {
+        "th": th_id,
+        "path": "/ztp-fttb:ztp-fttb{192.168.85.11}/new-vs-rma",
+        "leafs": ["new"]
     }
+}
 
-    configuration = {
-        "l3vpn": {
-            "name": "volvo",
-            "as-number": "65101",
-            "endpoints": [
-                {
-                    "name": "main-office",
-                    "ce-device": "ce0",
-                    "ce-interface": "GigabitEthernet0/11",
-                    "ip-network": "10.10.1.0/24",
-                    "bandwidth": "12000000"
-                },
-                {
-                    "name": "branch-office1",
-                    "ce-device": "ce1",
-                    "ce-interface": "GigabitEthernet0/11",
-                    "ip-network": "10.7.7.0/24",
-                    "bandwidth": "6000000"
-                },
-                {
-                    "name": "branch-office2",
-                    "ce-device": "ce4",
-                    "ce-interface": "GigabitEthernet0/18",
-                    "ip-network": "10.8.8.0/24",
-                    "bandwidth": "300000"
-                }
-            ],
-            "qos-policy": "GOLD"
-        }
+req = requests.post(url, cookies=cookies_string, json=payload, verify=False)
+response = req.text
+ztp_entry = json.loads(response)
+ztp_entry["result"]["values"][0]["value"]
+
+# Validate the transaction changes
+payload = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "validate_commit",
+    "params": {"th": th_id}
+}
+
+req = requests.post(url, cookies=cookies_string, json=payload, verify=False)
+response = req.text
+
+# Commit the transaction changes
+payload = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "commit",
+    "params": {"th": th_id}
+}
+
+req = requests.post(url, cookies=cookies_string, json=payload, verify=False)
+response = req.text
+
+# Perform a dry-run-native of the commit
+payload = {
+    "jsonrpc": "2.0",
+    "id": 168,
+    "method": "commit",
+    "params": {"th": "th_id", "flags": ["dry-run=native"]}
+}
+
+# Get a list of keys for authentication users
+payload = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "get_list_keys",
+    "params": {"th": 1, "path": "/aaa/authentication/users/user"}
+}
+
+# Set a value for a specific path
+payload = {
+    "jsonrpc": "2.0",
+    "id": 562,
+    "method": "set_value",
+    "params": {
+        "path": "/acidc-core:acidc/tenants{tenant5}/acidc-pa:port/pa{pa6}/pod",
+        "value": "1",
+        "th": th_id
     }
-    print(x)
+}
 
-    response = requests.post(service_vpn3, json=configuration, headers=x)
+# Get specific values for an authentication user
+payload = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "get_values",
+    "params": {
+        "th": th_id,
+        "path": "/aaa/authentication/users/user{test}",
+        "leafs": ["uid", "gid", "password", "ssh_keydir", "homedir"]
+    }
+}
 
-    if response.status_code == 201:
-        print("Configuration created successfully!")
-    else:
-        print(f"Failed to create configuration. Status code: {response.status_code}")
-        print("Response content:")
-        print(response.reason)
-        print(1)
-        print(response.headers.get("Allow"))
-        print(2)
-        print(response.text)
+# Run an action
+payload = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "run_action",
+    "params": {"th": 1, "path": "/devices/sync-from"}
+}
 
+# Create a list entry
+payload = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "create",
+    "params": {"th": 2, "path": "/aaa/authentication/users/user{test}"}
+}
+
+# Show the configuration for a specific path
+payload = {
+    "jsonrpc": "2.0",
+    "id": 45,
+    "method": "show_config",
+    "params": {
+        "expand_mode": "none",
+        "mode": "config",
+        "path": "/ztp-fttb:ztp-fttb{1.1.1.1}",
+        "th": th_id,
+        "result_as": "json3"
+    }
+}
